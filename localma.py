933b923b99cbc81ac96188108bf7ea0dccf83c4f
kgonapa <[kasiviswanadh1008@gmail.com]>
2024-04-10 18:05:34 +0200

    final working model

---
+++ localma.py
@@ -0,0 +1,39 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms.ollama import Ollama
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+## Langmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+
+#prompt Template
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
+
+##Streamlit framework
+
+st.title('Langchain Demo With LLAMA API')
+input_text=st.text_input("Search the topic u want")
+
+#Ollama AI LLMs call
+llm = Ollama(model="llama2")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
